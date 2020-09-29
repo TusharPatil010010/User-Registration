9d4b6d6e59ac2fc45548c3fdf57ebf4698526a55
TusharPatil010010 <tusharp010010@gmail.com>
2020-09-30 00:26:54 +0530

    Added functionality to validate user's Password

--- UserRegistration.java
+++ UserRegistration.java
@@ -33,7 +33,7 @@ public class UserUserRegistration {
 		
 	}
 	private String validatePassword(String pass) {
-		String expression = "[A-Za-z0-9]{8,}";	//Pattern for Password
+		String expression = "(?=.*[A-Z])[A-Za-z0-9]{8,}";	//Pattern for Password
 		Pattern pattern = Pattern.compile(expression);
 		Matcher match = pattern.matcher(pass);
 		if(match.find()) {
