21e04e10d7b9f0f419d3b1f5cd6c90f0a1a0ef9a
TusharPatil010010 <tusharp010010@gmail.com>
2020-09-30 00:15:33 +0530

    Added functionality to validate user's last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,10 +2,23 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistration {
-	
+public class Registration {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Registration user = new Registration();
+		
+		System.out.println("Enter the first name");
+		String firstName = sc.nextLine();
+		System.out.println("Enter the last name");
+		String lastName = sc.nextLine();
+		
+		System.out.println(firstName + " is " +user.validateInput(firstName)); //Validating firstName
+		System.out.println(lastName + " is " +user.validateInput(lastName)); //Validating lastName
+		sc.close();
+	}
 	private String validateInput(String name) {
-		String expression = "(^[A-Z]{1})[a-z]{2,}$";
+		String expression = "(^[A-Z]{1})[a-z]{2,}$"; // Pattern for Names
 		Pattern pattern = Pattern.compile(expression);
 		Matcher match = pattern.matcher(name);
 		if(match.find()) {
@@ -13,16 +26,4 @@ public class UserRegistration {
 		}
 		return "invalid";
 	}
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		UserRegistration user = new UserRegistration();
-
-		System.out.println("Enter the first name");
-		String firstName = sc.nextLine();
-
-		System.out.println(firstName + " is " +user.validateInput(firstName));		
-		sc.close();
-	}
-
 }
