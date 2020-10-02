137ceaf60ff6ff4d6e80baf1a8e236db7b766f24
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-02 13:44:07 +0530

    Added functionality to validate sample emails provided with the help of junit test

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,71 +1,43 @@
+package com.Assignment.Demo;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-	private String validateInput(String name) {
-		String expression = "(^[A-Z]{1})[a-z]{2,}$"; // Pattern for Names
-		Pattern pattern = Pattern.compile(expression);
-		Matcher match = pattern.matcher(name);
-		if(match.find()) {
+	public String validateEmail(String check) {
+		String regex = "^[a-zA-Z0-9_]+[-+.]?[A-Za-z0-9_]+@[A-Za-z0-9]+[.][a-z]{2,}[.]?([a-z]{2,})?$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(check);
+		if(matcher.find()) {
 			return "valid";
 		}
 		return "invalid";
-	}
-	private String validateEmail(String Email) {
-		String expression = "(abc?[.][A-Za-z]*@bl[.]co[.][A-Za-z]{2,})$"; // Pattern for Email
-		Pattern pattern = Pattern.compile(expression);
-		Matcher match = pattern.matcher(Email);
-		if(match.find()) {
-			return "valid";
-		}
-		return "invalid";
-	}
-	private String validateNumber(String number) {
-		String expression = "^[0-9]{2}?[ ][0-9]{4,13}";	// Pattern for Number
-		Pattern pattern = Pattern.compile(expression);
-		Matcher match = pattern.matcher(number);
-		if(match.find()) {
-			return "valid";
-		}
-		return "invalid";
-		
-	}
-	private String validatePassword(String pass) {
-		String expression = "(?=.*[A-Z])(?=.*\\d)((?=.*[@$!%*?&]){1})[A-Za-z0-9]{8,}";	//Pattern for Password
-		Pattern pattern = Pattern.compile(expression);
-		Matcher match = pattern.matcher(pass);
-		if(match.find()) {
-			return "valid";
-		}
-		return "invalid";
-		
 	}
 	
-		public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		UserRegistration user = new UserRegistration();
-		
-		System.out.println("Enter the first name");
-		String firstName = sc.nextLine();
-		System.out.println("Enter the last name");
-		String lastName = sc.nextLine();
-		System.out.println("Enter the Email Id");
-		String Email = sc.nextLine();
-		System.out.println("Enter the Phone Number");
-		String Phone = sc.nextLine();
-		System.out.println("Enter the Password");
-		String Password = sc.nextLine();
-		
-		System.out.println(firstName + " is " +user.validateInput(firstName)); //Validating firstName
-		System.out.println(lastName + " is " +user.validateInput(lastName)); //Validating lastName
-		System.out.println(Email + " is " +user.validateEmail(Email));	// Validating Email
-		System.out.println(Phone+ " is " +user.validateNumber(Phone));	// Validating Number
-		System.out.println(Password+ " is " +user.validatePassword(Password));//Validating Password
-		
-		sc.close();
+	public static void main(String[] args) {
+		UserRegistration patternMatcher = new UserRegistration();
+
+		String a1 = "abc@yahoo.com";
+		System.out.println(a1 + "----->" + patternMatcher.validateEmail(a1));
+		String a2 = "abc-100@yahoo.com";
+		System.out.println(a2 + "----->" + patternMatcher.validateEmail(a2));
+		String a3 = "abc.100@yahoo.com";
+		System.out.println(a3 + "----->" + patternMatcher.validateEmail(a3));
+		String a4 = "abc111@abc.com";
+		System.out.println(a4 + "----->" + patternMatcher.validateEmail(a4));
+		String a5 = "abc-100@abc.net";
+		System.out.println(a5 + "----->" + patternMatcher.validateEmail(a5));
+		String a6 = "abc.100@abc.com.au";
+		System.out.println(a6 + "----->" + patternMatcher.validateEmail(a6));
+		String a7 = "abc@1.com";
+		System.out.println(a7 + "----->" + patternMatcher.validateEmail(a7));
+		String a8 = "abc@gmail.com.com";
+		System.out.println(a8 + "----->" + patternMatcher.validateEmail(a8));
+		String a9 = "abc+100@gmail.com";	
+		System.out.println(a9 + "----->" + patternMatcher.validateEmail(a9));
+
 	}
 	
 }
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -2,39 +2,44 @@ package com.Assignment.Demo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.*;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class UserRegistrationTest {
+	
+	private String email;
+	private String expectedValue;
+	
+	public UserRegistrationTest(String email, String expectedValue) {
+		
+		super();
+		this.email = email;
+		this.expectedValue = expectedValue;
+	}
+	
+	@Before
+	public void initialize() {
+		UserRegistration obj = new UserRegistration();
+	}
+	
+	@Parameterized.Parameters
+	public static Collection input() {
+		return Arrays.asList(new Object[][] {{"abc-100@yahoo.com", "valid"}, {"abc@yahoo.com", "valid"}, {"abc.100@yahoo.com", "valid"}, 
+		{"abc111@abc.com", "valid"}, {"abc-100@abc.net", "valid"}, {"abc.100@abc.com.au", "valid"}, {"abc@1.com", "valid"}, {"abc@gmail.com.com", "valid"}, 
+		{"abc+100@gmail.com", "valid"}});
+	}	
 
 	@Test
-	public void givenEntry_whenAllDataValid_thenReturnHappyMood() {
+	public void givenSamples_whenExpectedIsTrue_returnTrue() {
+		UserRegistration obj1 = new UserRegistration();
 		
-		UserRegistration obj = new UserRegistration();
-		
-		String name = obj.validateInput("Tushar");
-		String email = obj.validateEmail("abc.xyz@bl.co.in");
-		String number = obj.validateNumber("919876543210");
-		String password = obj.validatePassword("Qwerty@123");		
-	
-		assertTrue(name == "valid" && email == "valid" && number == "valid" && password == "valid");
-		System.out.println("Happy Mood");
-		
-			
-	}
-	
-	@Test
-	public void givenEntry_whenAllDataInalid_thenReturnSadMood() {
-		
-		UserRegistration obj = new UserRegistration();
-		
-		String name = obj.validateInput("Tushar");
-		String email = obj.validateEmail("abc.xyz@bl.co.in");
-		String number = obj.validateNumber("919876543210");
-		String password = obj.validatePassword("Qwerty@123");		
-	
-		assertTrue(name == "invalid" || email == "invalid" || number == "invalid" || password == "invalid");
-		System.out.println("Sad Mood");
-		
-			
-	}
+	    assertEquals(expectedValue, obj1.validateEmail(email));
+	        
+    }
 }
+
